b7beec24aafcbe86a9aac0c15cbcdc7d415d2d8e
Antoine Dausque <antoine.dausque@hotmail.fr>
2024-11-14 14:55:47 +0100

    fix bug compil

--- src/app/cockpit/cockpit.module.ts
+++ src/app/cockpit/cockpit.module.ts
@@ -1,12 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { IonicModule } from "@ionic/angular";
+import { CockpitPageRoutingModule } from "./cockpit-routing.module";
+import { CockpitPage } from "./cockpit.page";
 
-import { IonicModule } from '@ionic/angular';
-
-import { CockpitPageRoutingModule } from './cockpit-routing.module';
-
-import { CockpitPage } from './cockpit.page';
 
 @NgModule({
   imports: [
